b00290d8d9495ebadcaa476b25a3046794ae6a6a
kumrulislam1999 <kumrulislam1999@gmail.com>
2023-08-12 18:13:50 +0600

    gh

---
+++ javascript/active.js
@@ -0,0 +1,40 @@
+document.getElementById('demo12').innerHTML = 5 + 6;
+document.getElementById('demo12').style.display = 'block';
+
+//window.alert(5 + 6);
+//alert(10 * 5);
+
+//console.log(5 + 8);
+
+document.getElementById('demo14').innerHTML = "Hello Doly.";
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+document.getElementById('demo15').innerHTML = c;
+
+{
+    let a, b, c;
+    a = 15; b = 10; c = a + b;
+    document.getElementById('demo16').innerHTML = c;
+}
+
+document.getElementById('demo17').innerHTML =
+    "hello Dolly!";
+
+function myFunction() {
+    document.getElementById('demo18').innerHTML = 'hello Dolly';
+    document.getElementById("demo19").innerHTML = "How are you";
+}
+
+document.getElementById('demo20').innerHTML = 10.50;
+document.getElementById('demo21').innerHTML = 10;
+
+document.getElementById('demo22').innerHTML = 'Hellow World!';
+document.getElementById('demo23').innerHTML = "Hellow World!";
+
+function d_variable() {
+    let x;// ;This si a declaretion of a variable;
+    x = 10; // 10 is assigned a value in x;
+    document.getElementById('demo24').innerHTML = x;
+}
